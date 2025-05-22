d42ad48a56d48d57ac416d866df18a5cfab7c6db
Alex Bessa <alexbessa.dev@gmail.com>
2025-05-22 11:15:15 -0300

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [data, setData] = useState("");
+
+  // useEffect(() => {
+  //   console.log("Executa na primeira redenrização e em todas as re-renderizações..");
+  // });
+
+  // useEffect(() => {
+  //   console.log("Executa na primeira redenrização.");
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log("Executa na primeira redenrização e em todas as re-renderizações de um componete específico.");
+  // }, [count1]);
+
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     console.log("Executou depois de 1 segundo.");
+  //   }, 1000);
+
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   }
+  // }, [count1]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/alexgeracaotech")
+      .then(response => response.json())
+      .then(data => setData(data))
+
+    console.log(data);
+  }, []);
+
+
 
   return (
     <>
-
+      <div >
+        <span style={{ display: 'block' }}>{count1}</span>
+        <button onClick={() => setCount1(count1 + 1)}>counter1</button>
+      </div>
+      <div >
+        <span style={{ display: 'block' }}>{count2}</span>
+        <button onClick={() => setCount2(count2 + 1)}>counter2</button>
+      </div>
     </>
+
   );
 }
